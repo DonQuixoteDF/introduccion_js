be48a9607619230208ac47556ef95fc8a44dc1f8
DonQuixoteDF <114237145+DonQuixoteDF@users.noreply.github.com>
2022-10-13 21:55:38 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+//- Tu nombre (string)
+var nombre = Francisco
+
+//- Tu apellido (string)
+var apellido = Pereira
+//- Tu edad (number)
+var edad = 22
+
+//- ¿Eres desarrollador de aplicaciones web? (boolean)
+var desarrollador = true
+
+//- Tu fecha de nacimiento (Date)
+var birthday = "22 de Abril de 1993"
+
+//- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+var libroFavorito = {
+    titulo:"La Divina Comedia",
+    autor: "Dante Alighieri",
+    fecha: "+-1300"
+};
+
+//- Tu pasatiempos favoritos (Array)
+
+var pasatiempos = [
+    cocinar,
+    leer, 
+    música,
+    estudiar
+]
+
+//- Declara una funcion miActividadFavorita la cual retorne un string con la accion que mas te guste realizar */
+function miActividadFavorita() {
+    return cocinar
+}
